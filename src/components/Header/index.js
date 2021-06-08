dec1b28c1ceccac4c1075d11b3882cd7af1872c8
Mateus Alencar <mtauws@gmail.com>
2021-06-08 12:13:29 -0300

    chore: css of the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from '../../assets/logo.png';
+import './index.css';
 
 export default class Header extends Component {
   render() { 
     return (
       <header className="header">
-        <img src={logo} alt="Logo of the site" />
+        <img className="logo" src={logo} alt="Logo of the site" />
         <nav className="navbar">
-          <a className="link" href="#">My Projects</a>
+          <a href="github.com" className="link">My Projects</a>
           <button className="btn btn-hello">Say Hello</button>
         </nav>
       </header>
