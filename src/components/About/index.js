2f184397a4b86405e74df34708cb1eaf9fd185d8
Mateus Alencar <mtauws@gmail.com>
2021-09-17 18:13:50 -0300

    chore: structure of the section

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './index.css';
+
+export default class About extends Component {
+  render() {
+    return (
+      <div className="about">
+        <h2 className="about-title">Hi, I’m Mateus. Nice to meet you.</h2>
+        <p className="about-text">
+          I'm Mateus, a 21 years old Full-Stack Enginner from Brazil that loves
+          apply his knowledge to solve problems, create amazing products and
+          impact lives.
+        </p>
+      </div>
+    );
+  }
+}
