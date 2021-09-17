eedeb4f7eb79d370f047f51208f0323b3c50122b
Mateus Alencar <mtauws@gmail.com>
2021-09-17 18:50:45 -0300

    chore: structure of Hero Section

---
+++ src/components/Hero/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './index.css';
+
+export default class Hero extends Component {
+  render() {
+    return (
+      <div className="hero">
+        <h1 className="title-hero">Front-end Developer &#38; Designer</h1>
+      </div>
+    );
+  }
+}
