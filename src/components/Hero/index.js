9ea5000dcc3a239738ae8d77730861bfb6de0b5b
Mateus Alencar <mtauws@gmail.com>
2021-09-17 18:59:47 -0300

    chore: add assets of hero section

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Avatar from '../../assets/avatar.svg';
 import './index.css';
 
 export default class Hero extends Component {
@@ -6,6 +7,10 @@ export default class Hero extends Component {
     return (
       <div className="hero">
         <h1 className="title-hero">Front-end Developer &#38; Designer</h1>
+        <p className="text-hero">
+          I design and code beautifully simple things, and I love what I do.
+        </p>
+        <img src={Avatar} alt="My Avatar" />
       </div>
     );
   }
