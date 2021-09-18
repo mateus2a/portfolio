bda90242e02ceb6ade58a5a412f38a7b98e0358c
Mateus Alencar <mtauws@gmail.com>
2021-09-18 12:19:59 -0300

    chore: responsive header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,12 +3,14 @@ import logo from '../../assets/logo.png';
 import './index.css';
 
 export default class Header extends Component {
-  render() { 
+  render() {
     return (
       <header className="header">
         <img className="logo" src={logo} alt="Logo of the site" />
         <nav className="navbar">
-          <a href="github.com" className="link">My Projects</a>
+          <a href="github.com" className="link">
+            My Projects
+          </a>
           <button className="btn btn-hello">Say Hello</button>
         </nav>
       </header>
